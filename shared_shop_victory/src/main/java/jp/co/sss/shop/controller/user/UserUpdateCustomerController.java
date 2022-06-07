ccab3d2580dd8a21fe4ea9a59c98a52603402947
katsushiro <katsushiro.tis2022@gmail.com>
2022-06-07 15:34:37 +0900

    コントローラの追加

---
+++ shared_shop_victory/src/main/java/jp/co/sss/shop/controller/user/UserUpdateCustomerController.java
@@ -0,0 +1,18 @@
+package jp.co.sss.shop.controller.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import jp.co.sss.shop.repository.UserRepository;
+
+/**
+ * 会員管理　会員変更(一般会員)
+ *
+ * @author SystemShared
+ */
+@Controller
+public class UserUpdateCustomerController {
+
+	@Autowired
+	UserRepository userRepository;
+}
