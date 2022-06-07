37efa98ed6c937c5226aa6d0bca5b9565125ba1e
sakai543 <sakai.tis2022@gmail.com>
2022-06-07 14:39:53 +0900

    itirannhyouji

---
+++ src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
@@ -0,0 +1,49 @@
+package jp.co.sss.shop.controller.item;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sss.shop.bean.ItemBean;
+import jp.co.sss.shop.entity.Item;
+import jp.co.sss.shop.repository.ItemRepository;
+import jp.co.sss.shop.util.BeanCopy;
+import jp.co.sss.shop.util.Constant;
+
+/**
+ * 商品管理 一覧表示機能(一般会員用)のコントローラクラス
+ *
+ * @author SystemShared
+ */
+@Controller
+public class ItemShowCustomerController {
+	/**
+	 * 商品情報
+	 */
+	@Autowired
+	ItemRepository itemRepository;
+
+	/**
+	 * トップ画面 表示処理
+	 *
+	 * @param model Viewとの値受渡し
+	 * @return "/" トップ画面へ
+	 */
+	@RequestMapping(path = "/")
+	public String index(Model model) {
+		// 商品情報を全件検索(新着順)
+		List<Item> itemList = itemRepository.findByDeleteFlagOrderByInsertDateDescIdAsc(Constant.NOT_DELETED);
+
+		// エンティティ内の検索結果をJavaBeansにコピー
+		List<ItemBean> itemBeanList = BeanCopy.copyEntityToItemBean(itemList);
+
+		// 商品情報をViewへ渡す
+		model.addAttribute("items", itemBeanList);
+		model.addAttribute("url", "/item/list");
+		return "item/list/item_list";
+	}
+
+}
