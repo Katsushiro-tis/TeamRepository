503c84cc2a9f3632004459150a4660b52def17f3
katsushiro <katsushiro.tis2022@gmail.com>
2022-06-09 16:32:16 +0900

    会員変更、削除機能完成有能

--- shared_shop_victory/src/main/java/jp/co/sss/shop/controller/user/UserDeleteCustomerController.java
+++ shared_shop_victory/src/main/java/jp/co/sss/shop/controller/user/UserDeleteCustomerController.java
@@ -1,10 +1,21 @@
 package jp.co.sss.shop.controller.user;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.entity.User;
+import jp.co.sss.shop.form.UserForm;
 import jp.co.sss.shop.repository.UserRepository;
+import jp.co.sss.shop.util.Constant;
 
 /**
  * 会員管理　会員変更(一般会員)
@@ -13,16 +24,58 @@ import jp.co.sss.shop.repository.UserRepository;
  */
 @Controller
 public class UserDeleteCustomerController {
+	
 	@Autowired
 	UserRepository userRepository;
 	
-	@PostMapping("/user/delete/check")
-	public String userDeleteCheck() {
-		return "/user/delete/user_delete_check";
+	@Autowired
+	HttpSession session;
+	
+	@RequestMapping(path = "/user/delete/check", method = RequestMethod.POST)
+	public String userDeleteCheck(Model model, @ModelAttribute UserForm form) {
+
+		UserBean sessionUser = (UserBean) session.getAttribute("user") ;
+		form.setId(sessionUser.getId());
+		form.setAuthority(sessionUser.getAuthority());
+		
+		// 削除対象の会員情報を取得
+		User user = userRepository.getById(form.getId());
+		
+		UserBean userBean = new UserBean();
+
+		// Userエンティティの各フィールドの値をUserBeanにコピー
+		BeanUtils.copyProperties(user, userBean);
+
+		// 会員情報をViewに渡す
+		model.addAttribute("user", userBean);
+
+		System.out.println("確認画面入ります");
+		return "user/delete/user_delete_check";
 	}
 	
-	@PostMapping("/user/delete/complete")
+	
+	@RequestMapping(path = "/user/delete/complete", method = RequestMethod.POST)
+	public String userDeleteComplete(@ModelAttribute UserForm form) {
+
+		UserBean sessionUser = (UserBean) session.getAttribute("user") ;
+		form.setId(sessionUser.getId());
+		
+		// 削除対象の会員情報を取得
+		User user = userRepository.getById(form.getId());
+		
+		// 削除フラグを立てる
+		user.setDeleteFlag(Constant.DELETED);
+		
+		// 会員情報を保存
+		userRepository.save(user);
+
+		return "redirect:/user/delete/complete";
+	}
+	
+	
+	@GetMapping("/user/delete/complete")
 	public String userDeleteComplete() {
+		System.out.println("完了画面(get)入ります");
 		return "/user/delete/user_delete_complete";
 	}
 }
