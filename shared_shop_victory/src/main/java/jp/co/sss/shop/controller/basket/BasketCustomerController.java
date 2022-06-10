c98cab2e6117a98a975b771b91a06f21a373e327
宇留間黎 <rey.soccer70@au.com>
2022-06-10 10:52:26 +0900

    入力チェック追加

--- shared_shop_victory/src/main/java/jp/co/sss/shop/controller/basket/BasketCustomerController.java
+++ shared_shop_victory/src/main/java/jp/co/sss/shop/controller/basket/BasketCustomerController.java
@@ -3,16 +3,20 @@ package jp.co.sss.shop.controller.basket;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jp.co.sss.shop.bean.BasketBean;
+import jp.co.sss.shop.entity.Order;
 import jp.co.sss.shop.form.AddressForm;
 import jp.co.sss.shop.form.BasketForm;
 import jp.co.sss.shop.form.OrderForm;
@@ -73,41 +77,47 @@ public class BasketCustomerController {
 
 	// 届け先入力画面へ
 	@RequestMapping(path = "/address/input", method = RequestMethod.POST)
-	public String ShopOrderRegist() {
+	public String ShopOrderRegist(AddressForm addressform) {
+
 		return "order/regist/order_address_input";
 	}
 
 	// 支払い方法選択画面へ
 	@RequestMapping(path = "/payment/input", method = RequestMethod.POST)
-	public String PaymentInput(AddressForm addressform, HttpSession session) {
-//		session.setAttribute("payment", orderform.getPayMethod());
+	public String PaymentInput(@Valid @ModelAttribute AddressForm addressform, BindingResult result,
+			HttpSession session) {
 
-//		Order order = new Order();
-//		order.setPostalCode(addressform.getPostalCode());
-//		order.setAddress(addressform.getAddress());
-//		order.setName(addressform.getName());
-//		order.setPhoneNumber(addressform.getPhoneNumber());
-//		orderRepository.save(order);
+		Order order = new Order();
+		order.setPostalCode(addressform.getPostalCode());
+		order.setAddress(addressform.getAddress());
+		order.setName(addressform.getName());
+		order.setPhoneNumber(addressform.getPhoneNumber());
+
+		session.setAttribute("address", order);
+		session.setAttribute("form", addressform);
+
+		if (result.hasErrors()) {
+			return "order/regist/order_address_input";
+		}
 		return "order/regist/order_payment_input";
 	}
 
-	// 注文登録確認画面
+	// 注文登録確認画面へ
 	@RequestMapping(path = "/order/check", method = RequestMethod.POST)
 	public String OrderCheck(OrderForm oderform, HttpSession session) {
 
+		Order order = new Order();
+		order.setPayMethod(oderform.getPayMethod());
+
+		session.setAttribute("payment", oderform.getPayMethod());
 		return "order/regist/order_check";
 	}
 
 	// 注文登録完了画面
 	@RequestMapping(path = "/order/complete")
 	public String OrderComplete() {
-		return "order/regist/order_complete";
-	}
 
-	// トップ画面へ (とりあえず）
-	@RequestMapping(path = "/go/top")
-	public String GoTop() {
-		return "redirect:/";
+		return "order/regist/order_complete";
 	}
 
 	@PostMapping("/basket/test")
