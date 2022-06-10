990eb4d7542d39ce22c5f02592a9a0e7d44ed37a
katsushiro <katsushiro.tis2022@gmail.com>
2022-06-10 13:59:28 +0900

    売れ筋順メソッド

--- shared_shop_victory/src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
+++ shared_shop_victory/src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -110,6 +111,12 @@ public class ItemShowCustomerController {
 	public String showFavoriteList(Model model) {
 
 		return "/item/list/item_favorite";
+	}
+	
+	
+	@GetMapping("/item/list/{sortType}")
+	public String itemListSortType() {
+		return "item_list";
 	}
 
 }
--- shared_shop_victory/src/main/java/jp/co/sss/shop/repository/OrderItemRepository.java
+++ shared_shop_victory/src/main/java/jp/co/sss/shop/repository/OrderItemRepository.java
@@ -1,6 +1,9 @@
 package jp.co.sss.shop.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import jp.co.sss.shop.entity.OrderItem;
 
@@ -10,4 +13,12 @@ import jp.co.sss.shop.entity.OrderItem;
  * @author System Shared
  */
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+	
+	@Query("SELECT oi.item, sum(oi.quantity) "
+			+ "FROM OrderItem oi "
+//			+ "LEFT JOIN item i ON oi.item = i.id"
+//			+ "WHERE i.deleteFlag GROUP BY oi.item"
+			+ "ORDER BY sum(oi.quantity)"
+			+ "")
+	public List<OrderItem> findByDeleteFlag(int deleteFlag);
 }
