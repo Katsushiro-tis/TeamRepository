d24ca2f497594cda6e10b23f220b4c847be1e5c5
宇留間黎 <rey.soccer70@au.com>
2022-06-17 09:23:40 +0900

    調整

--- shared_shop_victory/src/main/java/jp/co/sss/shop/controller/order/OrderRegistCustomerController.java
+++ shared_shop_victory/src/main/java/jp/co/sss/shop/controller/order/OrderRegistCustomerController.java
@@ -181,6 +181,7 @@ public class OrderRegistCustomerController {
 			// iのオーダーを取得
 			orderItemList.get(i);
 			// OrderItemBean型のorderItemBeanを作成
+
 			OrderItemBean orderItemBean = new OrderItemBean();
 			// orderItemBeanへ i のオーダーを代入
 			orderItemBean = orderItemList.get(i);
