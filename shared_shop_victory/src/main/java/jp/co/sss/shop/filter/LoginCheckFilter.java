b5175cc2fe10bd24b29664d89c0736bcc96c1c9c
藤﨑 <fujisaki.tis2022@gmail.com>
2022-06-20 15:27:52 +0900

    aaaaaaa

--- shared_shop_victory/src/main/java/jp/co/sss/shop/filter/LoginCheckFilter.java
+++ shared_shop_victory/src/main/java/jp/co/sss/shop/filter/LoginCheckFilter.java
@@ -66,12 +66,7 @@ public class LoginCheckFilter implements Filter {
 				&& !requestURL.endsWith("/user/regist/input") && !requestURL.endsWith("/user/regist/check")
 				&& !requestURL.endsWith("/user/regist/complete") || requestURL.indexOf("/basket") != -1
 				|| requestURL.indexOf("/order") != -1) {
-<<<<<<< HEAD
-			System.out.println("非会員用実行対象だよ : " + requestURL);
-			// URLのリクエスト先がフィルタ実行対象である場合
-=======
 			// URLのリクエスト先がフィルタ実行対象の場合場合
->>>>>>> 5a89f4dd09768eb761352a589c7157ae35ff3d94
 			return true;
 		} else {
 			// URLのリクエスト先がフィルタ実行対象ではない場合
