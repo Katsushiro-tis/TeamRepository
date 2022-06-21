1234e15ae420280aab57d59783a5d7f3dfb8f542
sakai543 <sakai.tis2022@gmail.com>
2022-06-21 11:21:23 +0900

    saidebarの入力チェック

---
+++ shared_shop_victory/src/main/java/jp/co/sss/shop/form/ItemPriceForm.java
@@ -0,0 +1,34 @@
+package jp.co.sss.shop.form;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * 商品情報のフォーム
+ *
+ * @author SystemShared
+ */
+public class ItemPriceForm {
+
+	/**
+	 * 価格
+	 */
+
+	@NotNull
+	private int itemMinPrice;
+	public int getItemMinPrice() {
+		return itemMinPrice;
+	}
+	public void setItemMinPrice(int itemMinPrice) {
+		this.itemMinPrice = itemMinPrice;
+	}
+	public int getItemMaxPrice() {
+		return itemMaxPrice;
+	}
+	public void setItemMaxPrice(int itemMaxPrice) {
+		this.itemMaxPrice = itemMaxPrice;
+	}
+	@NotNull
+	private int itemMaxPrice;
+
+
+}
